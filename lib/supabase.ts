5a55b04e5f9372a794eb30faf440dc7fb0a4f935
Rahul Bhagwat <rahulmbme@gmail.com>
2025-02-27 22:58:09 +0530

    persist auth on ios

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,19 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Database } from './database.types';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
 
 export type Tables = Database['public']['Tables'];
 export type Topic = Tables['topics']['Row'];
